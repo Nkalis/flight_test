716e17c110974a570326a1c134bf5f057f3029ac
NK <n.kalis98@gmail.com>
2019-03-06 15:24:58 +0100

    Damnation

--- data_extractor.py
+++ data_extractor.py
@@ -1,11 +1,8 @@
-def data_extractor(dataA, dataB):
+def data_extractor():
 
     import mat4py    
     
     data = mat4py.loadmat("matlab.mat")
     flightdata = data.get('flightdata', {})
     
-    data1 = flightdata.get(str(dataA))
-    data2 = flightdata.get(str(dataB))
-    
-    return data1, data2
+    return flightdata
--- map_plot.py
+++ map_plot.py
@@ -1,14 +1,12 @@
 import matplotlib.pyplot as plt
-import matplotlib.cm
 import numpy as np
 import itertools
 
 from mpl_toolkits.basemap import Basemap
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-from matplotlib.colors import Normalize
-
 from data_extractor import data_extractor
+
+'''Extracting the flight data from matlab.mat'''
+flightdata = data_extractor()
 
 ''' Setting up the map boundaries'''
 westlimit = 3.06
@@ -29,6 +27,7 @@ m.fillcontinents(color='#f2f2f2',lake_color='#46bcec')
 m.drawcountries(color = "black")
 m.drawcoastlines()
 
+
 '''Location of Rotterdam Airport'''
 rotlat = 51.9555
 rotlon = 4.4399
@@ -37,16 +36,16 @@ xpt, ypt = m(rotlon, rotlat)
 lonpt, latpt = m(xpt, ypt, inverse=True)
 m.plot(xpt, ypt, marker='D',color='r')
 
-''' Flight GPS Data '''
 
-lon = list(itertools.chain.from_iterable(data_extractor("Gps_long", "Gps_lat")[0].get('data')))
-lat = list(itertools.chain.from_iterable(data_extractor("Gps_long", "Gps_lat")[1].get('data')))
+''' Flight GPS Data '''
+lon = np.asarray(list(itertools.chain.from_iterable(flightdata.get('Gps_long').get('data'))))
+lat = np.asarray(list(itertools.chain.from_iterable(flightdata.get('Gps_lat').get('data'))))
+
+alt = np.asarray(list(itertools.chain.from_iterable(flightdata.get('Dadc1_bcAlt').get('data'))))
+time = flightdata.get('time').get('data')
 
 xpts, ypts = m(lon, lat)
 lonpts, latpts = m(xpts, ypts, inverse=True)
-m.plot(xpts, ypts)
+m.plot(xpts, ypts, color = 'm')
 
-#xpts, ypts = m(rotlon, rotlat)
-#m.scatter(xpt, ypt, marker='D',color='m')
-
-#plt.show()
+plt.show()
