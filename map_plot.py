fc7a44dbf8093f9ab63c2f2eb81c9eb6f88f388d
NK <n.kalis98@gmail.com>
2019-03-06 12:04:05 +0100

    meepmeep

--- map_plot.py
+++ map_plot.py
@@ -6,12 +6,18 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from matplotlib.colors import Normalize
 
+westlimit = 3.06
+southlimit = 51.23
+
+eastlimit = 6.70
+northlimit = 52.56
+
 fig, ax = plt.subplots(figsize=(10,20))
 
-m = Basemap(resolution='c', # c, l, i, h, f or None
+m = Basemap(resolution='h', # c, l, i, h, f or None
             projection='merc',
-            lat_0=54.5, lon_0=-4.36,
-            llcrnrlon=-6., llcrnrlat= 49.5, urcrnrlon=2., urcrnrlat=55.2)
+            lat_0=(westlimit+eastlimit)/2, lon_0=(southlimit+northlimit)/2,
+            llcrnrlon=westlimit, llcrnrlat= southlimit, urcrnrlon=eastlimit, urcrnrlat=northlimit)
 
 m.drawmapboundary(fill_color='#46bcec')
 m.fillcontinents(color='#f2f2f2',lake_color='#46bcec')
