020862ab303756bb10d1a22af67cbe0980524b90
Danny <danny.g@live.nl>
2019-03-12 15:32:57 +0100

    Auto stash before merge of "master" and "origin/master"
    added state space

---
+++ state_space_con.py
@@ -0,0 +1,20 @@
+def state_space_con(P,Q,R,sym_or_asym):
+    import numpy as np
+    import control.matlab as c
+    
+    if sym_or_asym == "sym":
+        A = -np.linalg.inv(P)*Q
+        B = -np.linalg.inv(P)*R
+        C = np.identity(4)
+        D = np.zeros(4,1)
+        sys = c.ss(A,B,C,D)
+        return sys
+    elif sym_or_asym == "asym":
+        A = -np.linalg.inv(P)*Q
+        B = -np.linalg.inv(P)*R
+        C = np.identity(4)
+        D = np.zeros(4,2)
+        sys = c.ss(A,B,C,D)
+        return sys
+    else:
+        return print("Please tell me if it is symmetric (sym) or asymmetric (asym)")
