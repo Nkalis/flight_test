28eb660fdf64adf56d2d72f468ae70f152efe485
Danny <danny.g@live.nl>
2019-03-12 16:50:07 +0100

    Added comments

--- state_space_con.py
+++ state_space_con.py
@@ -2,18 +2,20 @@ def state_space_conv(P,Q,R,sym_or_asym):
     import numpy as np
     import control.matlab as c
     
+    #Symmetrical Case
     if sym_or_asym == "sym":
         A = -np.linalg.inv(P)*Q
         B = -np.linalg.inv(P)*R
         C = np.identity(4)
-        D = np.zeros((4,1))
+        D = np.zeros((4,1)) #Only elevator
         sys = c.ss(A,B,C,D)
         return sys
+    #Asymmetrical Case
     elif sym_or_asym == "asym":
         A = -np.linalg.inv(P)*Q
         B = -np.linalg.inv(P)*R
         C = np.identity(4)
-        D = np.zeros((4,2))
+        D = np.zeros((4,2)) #Because both aileron and rudder displacement
         sys = c.ss(A,B,C,D)
         return sys
     else:
