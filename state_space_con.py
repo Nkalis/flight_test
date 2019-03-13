211077d5892b87bb8fd34971b9ee9415d20a9768
Danny <danny.g@live.nl>
2019-03-13 10:32:05 +0100

    Eigenvalues/stability check added

--- state_space_con.py
+++ state_space_con.py
@@ -9,7 +9,19 @@ def state_space_conv(P,Q,R,sym_or_asym):
         C = np.identity(4)
         D = np.zeros((4,1)) #Only elevator
         sys = c.ss(A,B,C,D)
-        return sys
+        eig = np.linalg.eigvals(A)
+        count = 0
+        for i in range(len(eig)-1):
+            if eig[i]<0:
+                count=count+1
+            else:
+                break
+        if count == len(eig):
+            print("System is stable")
+            return sys,eig
+        else:
+            print("System is unstable")
+            return sys,eig
     #Asymmetrical Case
     elif sym_or_asym == "asym":
         A = -np.linalg.inv(P)*Q
@@ -17,6 +29,18 @@ def state_space_conv(P,Q,R,sym_or_asym):
         C = np.identity(4)
         D = np.zeros((4,2)) #Because both aileron and rudder displacement
         sys = c.ss(A,B,C,D)
-        return sys
+        eig = np.linalg.eigvals(A)
+        count = 0
+        for i in range(len(eig)-1):
+            if eig[i]<0:
+                count=count+1
+            else:
+                break
+        if count == len(eig):
+            print("System is stable")
+            return sys,eig
+        else:
+            print("System is unstable")
+            return sys,eig
     else:
         return print("Please tell me if it is symmetric (sym) or asymmetric (asym)")
